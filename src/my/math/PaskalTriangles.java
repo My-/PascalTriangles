679d2da4b003e4f531f6a4abc7755e1a1cf08f6a
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-01-24 00:00:45 +0000

    rows loop depend on arr length

--- src/my/math/PaskalTriangles.java
+++ src/my/math/PaskalTriangles.java
@@ -6,17 +6,15 @@ public class PaskalTriangles {
      */
     public static int[][] pascalTriangle(final int n){
         int[][] p = new int[n][n];
-        //p[1] = new int[]{1};
+//        p[1] = new int[]{1};
         p[0][0] = 1;
-//        p[1][0] = 1;
-//        p[1][1] = 1;
 
         for(int y = 1; y < p.length; y++){
             int middle = y / 2 +(y %2 == 0 ? 1 : 0);
             p[y][0] = 1;
+//            p[y] = new int[middle +y/2];
 
-            for(int x = 1; x < n -1; x++){
-                //System.out.println("y: "+ y +", x: "+ x +", middle: "+ middle);
+            for(int x = 1; x < p[y].length; x++){
                 int value = p[y -1][x -1] +p[y -1][x];
                 p[y][x] = value;
             }
@@ -33,7 +31,7 @@ public class PaskalTriangles {
             System.out.print(String.format("%"+ offset +"s", " "));
 
             for(int i = 0; i < arr[row].length; i++ ){
-                if( arr[row][i] == 0 ){ break; }
+//                if( arr[row][i] == 0 ){ break; }
                 System.out.print(String.format("%6d", arr[row][i]));
             }
             System.out.println();
