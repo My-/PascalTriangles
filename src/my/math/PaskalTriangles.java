6036e722f8ac99300ee94e640b6a7adeb4050a27
Mindaugas Sharskus <minde.pinde+my-@gmail.com>
2018-01-24 00:16:48 +0000

    WORKS: smaller array

--- src/my/math/PaskalTriangles.java
+++ src/my/math/PaskalTriangles.java
@@ -5,16 +5,17 @@ public class PaskalTriangles {
      *   method outputs Pascal triange of given value
      */
     public static int[][] pascalTriangle(final int n){
-        int[][] p = new int[n][n];
+        int[][] p = new int[n][];
 //        p[1] = new int[]{1};
-        p[0][0] = 1;
+//        p[0] = new int[1];
+//        p[0][0] = 1;
 
-        for(int y = 1; y < p.length; y++){
-            int middle = y / 2 +(y %2 == 0 ? 1 : 0);
+        for(int y = 0; y < p.length; y++){
+//            int middle = y / 2 +(y %2 == 0 ? 1 : 0);
+            p[y] = new int[y +1];
             p[y][0] = 1;
-//            p[y] = new int[middle +y/2];
 
-            for(int x = 1; x < p[y].length; x++){
+            for(int x = 1; x < p[y].length-1; x++){
                 int value = p[y -1][x -1] +p[y -1][x];
                 p[y][x] = value;
             }
